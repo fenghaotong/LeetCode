e6ed4da0eec2aba907624555a8a043b728789df8
fenghaotong@datagrand.com <>
2021-04-20 21:06:47 +0800

    lengthOfLongestSubstring

--- src/longestsubstringwithoutrepeating/LongestSubstringWithoutRepeating.java
+++ src/longestsubstringwithoutrepeating/LongestSubstringWithoutRepeating.java
@@ -1,5 +1,8 @@
 package longestsubstringwithoutrepeating;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestSubstringWithoutRepeating {
     public static void main(String[] args) {
         String s = "abcabcab";
@@ -8,6 +11,24 @@ public class LongestSubstringWithoutRepeating {
 
     public static int lengthOfLongestSubstring(String s) {
         if(s.length() <=1) return s.length();
-        return 0;
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet<Character>();
+        int n = s.length();
+        // 右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // 左指针向右移动一格，移除一个字符
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
     }
 }
