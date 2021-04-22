dd2c47b7160753eaa2621a342f7f48546d1b21cb
fenghaotong@datagrand.com <fenghaotong@datagrand.com>
2021-04-22 18:07:27 +0800

    add longestPalindrome description

---
+++ src/longestpalindrome/LongestPalindrome.java
@@ -0,0 +1,10 @@
+package longestpalindrome;
+
+public class LongestPalindrome {
+    public static void main(String[] args) {
+
+    }
+    public static String longestPalindrome(String s) {
+        return "";
+    }
+}
