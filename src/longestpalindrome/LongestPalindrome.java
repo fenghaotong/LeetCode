32d64ea77a9a1945a96821b4077aa48ac1bb0521
Administrator <fenghaotong@datagrand.com>
2021-04-24 23:04:09 +0800

    longestPalindrome

--- src/longestpalindrome/LongestPalindrome.java
+++ src/longestpalindrome/LongestPalindrome.java
@@ -2,9 +2,33 @@ package longestpalindrome;
 
 public class LongestPalindrome {
     public static void main(String[] args) {
-
+        String s = "abab";
+        System.out.println(longestPalindrome(s));
     }
     public static String longestPalindrome(String s) {
-        return "";
+        if (s.length() < 2) return s;
+
+        int start = 0;
+        int end = 0;
+
+        for(int i = 0; i < s.length(); i ++){
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public static int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            --left;
+            ++right;
+        }
+        return right - left - 1;
     }
 }
