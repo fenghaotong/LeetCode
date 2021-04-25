d6e86a2d72e08f2f228b2fa4420e99868eaae9ef
fenghaotong@datagrand.com <fenghaotong@datagrand.com>
2021-04-25 13:27:46 +0800

    longestPalindrome change

--- src/longestpalindrome/LongestPalindrome.java
+++ src/longestpalindrome/LongestPalindrome.java
@@ -2,26 +2,25 @@ package longestpalindrome;
 
 public class LongestPalindrome {
     public static void main(String[] args) {
-        String s = "abab";
+        String s = "ababac";
         System.out.println(longestPalindrome(s));
     }
     public static String longestPalindrome(String s) {
         if (s.length() < 2) return s;
 
         int start = 0;
-        int end = 0;
+        int maxLen = 0;
 
         for(int i = 0; i < s.length(); i ++){
             int len1 = expandAroundCenter(s, i, i);
             int len2 = expandAroundCenter(s, i, i + 1);
             int len = Math.max(len1, len2);
-            if (len > end - start) {
+            if (len > maxLen) {
                 start = i - (len - 1) / 2;
-                end = i + len / 2;
+                maxLen = len;
             }
-
         }
-        return s.substring(start, end + 1);
+        return s.substring(start, start + maxLen);
     }
 
     public static int expandAroundCenter(String s, int left, int right) {
