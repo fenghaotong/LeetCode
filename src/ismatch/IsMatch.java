6580b15675e397c2416aee04613da465a437fbb5
fenghaotong <fenghaotong@datagrand.com>
2021-04-25 13:40:25 +0800

    Develop (#5)
    * change findMedianSortedArrays README
    
    * add longestPalindrome description
    
    * longestPalindrome
    
    * longestPalindrome change
    
    * ismatch description

---
+++ src/ismatch/IsMatch.java
@@ -0,0 +1,10 @@
+package ismatch;
+
+public class IsMatch {
+    public static void main(String[] args) {
+
+    }
+    public static boolean isMatch(String s, String p) {
+        return false;
+    }
+}
--- src/longestpalindrome/LongestPalindrome.java
+++ src/longestpalindrome/LongestPalindrome.java
@@ -2,9 +2,32 @@ package longestpalindrome;
 
 public class LongestPalindrome {
     public static void main(String[] args) {
-
+        String s = "ababac";
+        System.out.println(longestPalindrome(s));
     }
     public static String longestPalindrome(String s) {
-        return "";
+        if (s.length() < 2) return s;
+
+        int start = 0;
+        int maxLen = 0;
+
+        for(int i = 0; i < s.length(); i ++){
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > maxLen) {
+                start = i - (len - 1) / 2;
+                maxLen = len;
+            }
+        }
+        return s.substring(start, start + maxLen);
+    }
+
+    public static int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            --left;
+            ++right;
+        }
+        return right - left - 1;
     }
 }
